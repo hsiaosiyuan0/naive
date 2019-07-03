8af6e6c6db79dc57e6f4dab063548875e9e585a4
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2019-07-03 20:09:55 +0800

    interp update

--- src/lib/interpreter.rs
+++ src/lib/interpreter.rs
@@ -1,172 +1,194 @@
 use crate::ast::*;
 use crate::lexer::*;
+use crate::object::*;
 use crate::parser::*;
 use crate::token::*;
 use crate::visitor::AstVisitor;
+use std::collections::HashMap;
 
 pub struct Interpreter {}
 
 pub struct RuntimeError {}
 
-impl AstVisitor<(), RuntimeError> for Interpreter {
-  fn prog(&mut self, prog: &Prog) -> Result<(), RuntimeError> {
+impl AstVisitor<Option<JSObjPtr>, RuntimeError> for Interpreter {
+  fn prog(&mut self, prog: &Prog) -> Result<Option<JSObjPtr>, RuntimeError> {
+    let mut ret = Ok(None);
     for stmt in &prog.body {
-      match self.stmt(stmt) {
+      ret = match self.stmt(stmt) {
         Err(e) => return Err(e),
-        _ => (),
+        Ok(v) => Ok(v),
       }
     }
-    Ok(())
+    ret
   }
 
-  fn block_stmt(&mut self, stmt: &BlockStmt) -> Result<(), RuntimeError> {
+  fn block_stmt(&mut self, stmt: &BlockStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn var_dec_stmt(&mut self, stmt: &VarDec) -> Result<(), RuntimeError> {
+  fn var_dec_stmt(&mut self, stmt: &VarDec) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn empty_stmt(&mut self, stmt: &EmptyStmt) -> Result<(), RuntimeError> {
+  fn empty_stmt(&mut self, stmt: &EmptyStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn expr_stmt(&mut self, stmt: &ExprStmt) -> Result<(), RuntimeError> {
+  fn expr_stmt(&mut self, stmt: &ExprStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     self.expr(&stmt.expr)
   }
 
-  fn if_stmt(&mut self, stmt: &IfStmt) -> Result<(), RuntimeError> {
+  fn if_stmt(&mut self, stmt: &IfStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn for_stmt(&mut self, stmt: &ForStmt) -> Result<(), RuntimeError> {
+  fn for_stmt(&mut self, stmt: &ForStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn for_in_stmt(&mut self, stmt: &ForInStmt) -> Result<(), RuntimeError> {
+  fn for_in_stmt(&mut self, stmt: &ForInStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn do_while_stmt(&mut self, stmt: &DoWhileStmt) -> Result<(), RuntimeError> {
+  fn do_while_stmt(&mut self, stmt: &DoWhileStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn while_stmt(&mut self, stmt: &WhileStmt) -> Result<(), RuntimeError> {
+  fn while_stmt(&mut self, stmt: &WhileStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn cont_stmt(&mut self, stmt: &ContStmt) -> Result<(), RuntimeError> {
+  fn cont_stmt(&mut self, stmt: &ContStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn break_stmt(&mut self, stmt: &BreakStmt) -> Result<(), RuntimeError> {
+  fn break_stmt(&mut self, stmt: &BreakStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn ret_stmt(&mut self, stmt: &ReturnStmt) -> Result<(), RuntimeError> {
+  fn ret_stmt(&mut self, stmt: &ReturnStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn with_stmt(&mut self, stmt: &WithStmt) -> Result<(), RuntimeError> {
+  fn with_stmt(&mut self, stmt: &WithStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn switch_stmt(&mut self, stmt: &SwitchStmt) -> Result<(), RuntimeError> {
+  fn switch_stmt(&mut self, stmt: &SwitchStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn throw_stmt(&mut self, stmt: &ThrowStmt) -> Result<(), RuntimeError> {
+  fn throw_stmt(&mut self, stmt: &ThrowStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn try_stmt(&mut self, stmt: &TryStmt) -> Result<(), RuntimeError> {
+  fn try_stmt(&mut self, stmt: &TryStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn debug_stmt(&mut self, stmt: &DebugStmt) -> Result<(), RuntimeError> {
+  fn debug_stmt(&mut self, stmt: &DebugStmt) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn fn_stmt(&mut self, stmt: &FnDec) -> Result<(), RuntimeError> {
+  fn fn_stmt(&mut self, stmt: &FnDec) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn member_expr(&mut self, expr: &MemberExpr) -> Result<(), RuntimeError> {
+  fn member_expr(&mut self, expr: &MemberExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn new_expr(&mut self, expr: &NewExpr) -> Result<(), RuntimeError> {
+  fn new_expr(&mut self, expr: &NewExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn call_expr(&mut self, expr: &CallExpr) -> Result<(), RuntimeError> {
+  fn call_expr(&mut self, expr: &CallExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn unary_expr(&mut self, expr: &UnaryExpr) -> Result<(), RuntimeError> {
+  fn unary_expr(&mut self, expr: &UnaryExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn binary_expr(&mut self, expr: &BinaryExpr) -> Result<(), RuntimeError> {
+  fn binary_expr(&mut self, expr: &BinaryExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
+    let lhs = match self.expr(&expr.left) {
+      Ok(v) => v.unwrap(),
+      Err(e) => return Err(e),
+    };
+    let rhs = match self.expr(&expr.right) {
+      Ok(v) => v.unwrap(),
+      Err(e) => return Err(e),
+    };
+    let op = expr.op.symbol_data();
+    match op.kind {
+      Symbol::Add => Ok(Some(JSObject::add(lhs, rhs))),
+      _ => Ok(Some(js_undef())),
+    }
+  }
+
+  fn assign_expr(&mut self, expr: &AssignExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn assign_expr(&mut self, expr: &AssignExpr) -> Result<(), RuntimeError> {
+  fn cond_expr(&mut self, expr: &CondExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn cond_expr(&mut self, expr: &CondExpr) -> Result<(), RuntimeError> {
+  fn seq_expr(&mut self, expr: &SeqExpr) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn seq_expr(&mut self, expr: &SeqExpr) -> Result<(), RuntimeError> {
+  fn this_expr(&mut self, expr: &ThisExprData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn this_expr(&mut self, expr: &ThisExprData) -> Result<(), RuntimeError> {
+  fn id_expr(&mut self, expr: &IdData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn id_expr(&mut self, expr: &IdData) -> Result<(), RuntimeError> {
+  fn array_literal(&mut self, expr: &ArrayData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn array_literal(&mut self, expr: &ArrayData) -> Result<(), RuntimeError> {
+  fn object_literal(&mut self, expr: &ObjectData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn object_literal(&mut self, expr: &ObjectData) -> Result<(), RuntimeError> {
+  fn paren_expr(&mut self, expr: &ParenData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn paren_expr(&mut self, expr: &ParenData) -> Result<(), RuntimeError> {
+  fn fn_expr(&mut self, expr: &FnDec) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn fn_expr(&mut self, expr: &FnDec) -> Result<(), RuntimeError> {
+  fn regexp_expr(&mut self, expr: &RegExpData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn regexp_expr(&mut self, expr: &RegExpData) -> Result<(), RuntimeError> {
+  fn null_expr(&mut self, expr: &NullData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn null_expr(&mut self, expr: &NullData) -> Result<(), RuntimeError> {
+  fn str_expr(&mut self, expr: &StringData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn str_expr(&mut self, expr: &StringData) -> Result<(), RuntimeError> {
+  fn bool_expr(&mut self, expr: &BoolData) -> Result<Option<JSObjPtr>, RuntimeError> {
     unimplemented!()
   }
 
-  fn bool_expr(&mut self, expr: &BoolData) -> Result<(), RuntimeError> {
-    unimplemented!()
+  fn num_expr(&mut self, expr: &NumericData) -> Result<Option<JSObjPtr>, RuntimeError> {
+    let ret = JSNumber::from_str(&expr.value) as JSObjPtr;
+    Ok(Some(ret))
   }
+}
 
-  fn num_expr(&mut self, expr: &NumericData) -> Result<(), RuntimeError> {
-    println!("{:#?}", expr);
-    Ok(())
-  }
+pub struct Env {
+  outer: Option<Box<Env>>,
+  inner: Vec<Env>,
+  upvals: Vec<String>,
+  maps: HashMap<String, JSObjPtr>,
 }
 
 #[cfg(test)]
@@ -178,12 +200,16 @@ mod interp_tests {
   fn exec_test() {
     init_token_data();
 
-    let code = String::from("1");
+    let code = String::from("1 + 2");
     let src = Source::new(&code);
     let mut lexer = Lexer::new(src);
     let mut parser = Parser::new(&mut lexer);
     let mut interp = Interpreter {};
     let node = parser.prog().ok().unwrap();
-    interp.prog(&node);
+    let res = interp.prog(&node).ok().unwrap().unwrap();
+    JSObject::dump(res);
+
+    let v: f64 = "1e1".parse().unwrap();
+    println!("{:#?}", v);
   }
 }
--- src/lib/object.rs
+++ src/lib/object.rs
@@ -16,27 +16,36 @@ pub enum JSObjectType {
 
 pub struct RefCounting {}
 
+pub type JSObjPtr = *mut JSObject;
+pub type JSStrPtr = *mut JSString;
+pub type JSNumPtr = *mut JSNumber;
+pub type JSBoolPtr = *mut JSBoolean;
+pub type JSArrPtr = *mut JSArray;
+pub type JSDictPtr = *mut JSDict;
+pub type JSNullPtr = *mut JSNull;
+pub type JSUndefPtr = *mut JSUndefined;
+
 impl RefCounting {
-  pub fn ref_cnt(ptr: *mut JSObject) -> i32 {
+  pub fn ref_cnt(ptr: JSObjPtr) -> i32 {
     let obj = unsafe { Box::from_raw(ptr) };
     let rc = obj.ref_cnt;
     Box::into_raw(obj);
     rc
   }
 
-  pub fn inc(ptr: *mut JSObject) {
+  pub fn inc(ptr: JSObjPtr) {
     let mut obj = unsafe { Box::from_raw(ptr) };
     obj.ref_cnt += 1;
     Box::into_raw(obj);
   }
 
-  pub fn dec(ptr: *mut JSObject) {
+  pub fn dec(ptr: JSObjPtr) {
     let mut obj = unsafe { Box::from_raw(ptr) };
     obj.ref_cnt -= 1;
     RefCounting::x_release(Box::into_raw(obj));
   }
 
-  pub fn x_release(ptr: *mut JSObject) {
+  pub fn x_release(ptr: JSObjPtr) {
     let rc = RefCounting::ref_cnt(ptr);
     assert!(rc >= 0);
     if rc > 0 {
@@ -47,14 +56,14 @@ impl RefCounting {
     Box::into_raw(obj);
     match typ {
       JSObjectType::Array => {
-        let arr = unsafe { Box::from_raw(ptr as *mut JSArray) };
+        let arr = unsafe { Box::from_raw(ptr as JSArrPtr) };
         for item in &arr.a {
           RefCounting::dec(*item)
         }
         Box::into_raw(arr);
       }
       JSObjectType::Dict => {
-        let dict = unsafe { Box::from_raw(ptr as *mut JSDict) };
+        let dict = unsafe { Box::from_raw(ptr as JSDictPtr) };
         for (_, v) in &dict.d {
           RefCounting::dec(*v);
         }
@@ -74,16 +83,28 @@ pub struct JSObject {
 }
 
 impl JSObject {
-  pub fn is_type(ptr: *mut JSObject, typ: JSObjectType) -> bool {
+  pub fn is_type(ptr: JSObjPtr, typ: JSObjectType) -> bool {
     let obj = unsafe { Box::from_raw(ptr) };
     let t = obj.typ;
     Box::into_raw(obj);
     t == typ
   }
 
-  pub fn is_str(ptr: *mut JSObject) -> bool {
+  pub fn is_str(ptr: JSObjPtr) -> bool {
     JSObject::is_type(ptr, JSObjectType::String)
   }
+
+  pub fn add(lhs: JSObjPtr, rhs: JSObjPtr) -> JSObjPtr {
+    let lv = JSNumber::val(lhs as JSNumPtr);
+    let rv = JSNumber::val(rhs as JSNumPtr);
+    JSNumber::from_f64(lv + rv) as JSObjPtr
+  }
+
+  pub fn primitive() -> JSObjPtr {}
+
+  pub fn add_str() -> JSObjPtr {}
+
+  pub fn add_num() -> JSObjPtr {}
 }
 
 #[repr(C)]
@@ -91,6 +112,35 @@ impl JSObject {
 pub struct JSNumber {
   pub base: JSObject,
   pub v: f64,
+}
+
+impl JSNumber {
+  pub fn from_f64(v: f64) -> JSNumPtr {
+    Box::into_raw(Box::new(JSNumber {
+      base: JSObject {
+        ref_cnt: 1,
+        typ: JSObjectType::Number,
+      },
+      v,
+    }))
+  }
+
+  pub fn from_str(s: &str) -> JSNumPtr {
+    Box::into_raw(Box::new(JSNumber {
+      base: JSObject {
+        ref_cnt: 1,
+        typ: JSObjectType::Number,
+      },
+      v: s.parse().unwrap(),
+    }))
+  }
+
+  pub fn val(n: JSNumPtr) -> f64 {
+    let n = unsafe { Box::from_raw(n) };
+    let v = n.v;
+    Box::into_raw(n);
+    v
+  }
 }
 
 #[repr(C)]
@@ -101,7 +151,7 @@ pub struct JSString {
 }
 
 impl JSString {
-  pub fn new(s: &str) -> *mut JSString {
+  pub fn new(s: &str) -> JSStrPtr {
     Box::into_raw(Box::new(JSString {
       base: JSObject {
         ref_cnt: 1,
@@ -127,7 +177,7 @@ pub struct JSArray {
 }
 
 impl JSArray {
-  pub fn new() -> *mut JSArray {
+  pub fn new() -> JSArrPtr {
     Box::into_raw(Box::new(JSArray {
       base: JSObject {
         ref_cnt: 1,
@@ -137,14 +187,14 @@ impl JSArray {
     }))
   }
 
-  pub fn push(arr: *mut JSArray, item: *mut JSObject) {
+  pub fn push(arr: JSArrPtr, item: JSObjPtr) {
     let mut arr = unsafe { Box::from_raw(arr) };
     arr.a.push(item);
     RefCounting::inc(item);
     Box::into_raw(arr);
   }
 
-  pub fn pop(arr: *mut JSArray) -> *mut JSObject {
+  pub fn pop(arr: JSArrPtr) -> JSObjPtr {
     let mut arr = unsafe { Box::from_raw(arr) };
     unsafe {
       let ret = match arr.a.pop() {
@@ -164,11 +214,11 @@ impl JSArray {
 #[derive(Debug)]
 pub struct JSDict {
   pub base: JSObject,
-  pub d: HashMap<String, *mut JSObject>,
+  pub d: HashMap<String, JSObjPtr>,
 }
 
 impl JSDict {
-  pub fn new() -> *mut JSDict {
+  pub fn new() -> JSDictPtr {
     Box::into_raw(Box::new(JSDict {
       base: JSObject {
         ref_cnt: 1,
@@ -178,14 +228,14 @@ impl JSDict {
     }))
   }
 
-  pub fn set(map: *mut JSDict, k: &str, v: *mut JSObject) {
+  pub fn set(map: JSDictPtr, k: &str, v: JSObjPtr) {
     let mut dict = unsafe { Box::from_raw(map) };
     dict.d.insert(k.to_owned(), v);
     RefCounting::inc(v);
     Box::into_raw(dict);
   }
 
-  pub fn get(map: *mut JSDict, k: &str) -> *mut JSObject {
+  pub fn get(map: JSDictPtr, k: &str) -> JSObjPtr {
     let dict = unsafe { Box::from_raw(map) };
     unsafe {
       let ret = match dict.d.get(k) {
@@ -197,7 +247,7 @@ impl JSDict {
     }
   }
 
-  pub fn del(map: *mut JSDict, k: &str) -> *mut JSObject {
+  pub fn del(map: JSDictPtr, k: &str) -> JSObjPtr {
     let mut dict = unsafe { Box::from_raw(map) };
     unsafe {
       let v = match dict.d.remove(k) {
@@ -225,8 +275,16 @@ pub struct JSUndefined {
   pub base: JSObject,
 }
 
-pub static mut JS_NULL: *mut JSObject = null_mut();
-pub static mut JS_UNDEF: *mut JSObject = null_mut();
+pub static mut JS_NULL: JSObjPtr = null_mut();
+pub static mut JS_UNDEF: JSObjPtr = null_mut();
+
+pub fn js_null() -> JSObjPtr {
+  unsafe { JS_NULL }
+}
+
+pub fn js_undef() -> JSObjPtr {
+  unsafe { JS_UNDEF }
+}
 
 static INIT_JS_NULL_UNDEF_ONCE: Once = Once::new();
 pub fn init_js_null_undef() {
@@ -237,7 +295,7 @@ pub fn init_js_null_undef() {
         typ: JSObjectType::Null,
       },
     }));
-    JS_NULL = ptr as *mut JSObject;
+    JS_NULL = ptr as JSObjPtr;
 
     let ptr = Box::into_raw(Box::new(JSUndefined {
       base: JSObject {
@@ -245,48 +303,48 @@ pub fn init_js_null_undef() {
         typ: JSObjectType::Undefined,
       },
     }));
-    JS_UNDEF = ptr as *mut JSObject;
+    JS_UNDEF = ptr as JSObjPtr;
   });
 }
 
 impl JSObject {
-  pub fn dump(ptr: *mut JSObject) {
+  pub fn dump(ptr: JSObjPtr) {
     let obj = unsafe { Box::from_raw(ptr) };
     let typ = obj.typ;
     Box::into_raw(obj);
     match typ {
       JSObjectType::String => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSString) };
+        let obj = unsafe { Box::from_raw(ptr as JSStrPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Number => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSNumber) };
+        let obj = unsafe { Box::from_raw(ptr as JSNumPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Boolean => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSBoolean) };
+        let obj = unsafe { Box::from_raw(ptr as JSBoolPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Array => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSArray) };
+        let obj = unsafe { Box::from_raw(ptr as JSArrPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Dict => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSDict) };
+        let obj = unsafe { Box::from_raw(ptr as JSDictPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Null => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSDict) };
+        let obj = unsafe { Box::from_raw(ptr as JSNullPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
       JSObjectType::Undefined => {
-        let obj = unsafe { Box::from_raw(ptr as *mut JSObject) };
+        let obj = unsafe { Box::from_raw(ptr as JSUndefPtr) };
         println!("{:#?}", &obj);
         Box::into_raw(obj);
       }
@@ -304,21 +362,21 @@ mod object_tests {
 
     let s_ptr = JSString::new("hello world");
     let arr_ptr = JSArray::new();
-    JSArray::push(arr_ptr, s_ptr as *mut JSObject);
+    JSArray::push(arr_ptr, s_ptr as JSObjPtr);
 
-    JSObject::dump(s_ptr as *mut JSObject);
-    assert_eq!(2, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    JSObject::dump(s_ptr as JSObjPtr);
+    assert_eq!(2, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
     let s_ptr = JSArray::pop(arr_ptr);
-    JSObject::dump(s_ptr as *mut JSObject);
-    assert_eq!(1, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    JSObject::dump(s_ptr as JSObjPtr);
+    assert_eq!(1, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
-    JSArray::push(arr_ptr, s_ptr as *mut JSObject);
-    assert_eq!(2, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    JSArray::push(arr_ptr, s_ptr as JSObjPtr);
+    assert_eq!(2, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
-    RefCounting::dec(arr_ptr as *mut JSObject);
-    assert_eq!(1, RefCounting::ref_cnt(s_ptr as *mut JSObject));
-    JSObject::dump(s_ptr as *mut JSObject);
+    RefCounting::dec(arr_ptr as JSObjPtr);
+    assert_eq!(1, RefCounting::ref_cnt(s_ptr as JSObjPtr));
+    JSObject::dump(s_ptr as JSObjPtr);
   }
 
   #[test]
@@ -327,14 +385,14 @@ mod object_tests {
 
     let s_ptr = JSString::new("hello world");
     let dict_ptr = JSDict::new();
-    JSDict::set(dict_ptr, "test", s_ptr as *mut JSObject);
-    assert_eq!(2, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    JSDict::set(dict_ptr, "test", s_ptr as JSObjPtr);
+    assert_eq!(2, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
     JSDict::get(dict_ptr, "test");
-    assert_eq!(2, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    assert_eq!(2, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
     JSDict::del(dict_ptr, "test");
-    assert_eq!(1, RefCounting::ref_cnt(s_ptr as *mut JSObject));
+    assert_eq!(1, RefCounting::ref_cnt(s_ptr as JSObjPtr));
 
     unsafe {
       assert_eq!(JS_UNDEF, JSDict::get(dict_ptr, "test"));
